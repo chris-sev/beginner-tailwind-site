79ca2853e9179d905780b86ccff15d3c7e00e062
Chris on Code <hey@chrisoncode.io>
2020-09-04 14:53:42 -0700

    setup

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: ["src/**/*.html"],
+
+  theme: {
+    extend: {},
+  },
+  variants: {},
+  plugins: [],
+};
